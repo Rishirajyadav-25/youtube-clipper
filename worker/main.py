a5c51910ad76c8ef19aebc3733a6eb6589050b1b
Rishiraj-24 <rishirajcodes.23297020@gmail.com>
2025-12-30 18:57:40 +0530

    Deploying worker as web service

--- worker/main.py
+++ worker/main.py
@@ -1,20 +1,39 @@
-import asyncio
-from worker.queue import redis_client, QUEUE_NAME
-from worker.job_handler import process_job
+from worker.queue import listen_for_jobs
 
-
-async def worker_loop():
-    print("Worker started. Waiting for jobs...")
-
-    while True:
-        job = redis_client.brpop(QUEUE_NAME, timeout=5)
-
-        if job:
-            _, job_id = job
-            await process_job(job_id)
-        else:
-            await asyncio.sleep(1)
-
+def start_worker():
+    print("🚀 Worker started. Waiting for jobs...")
+    listen_for_jobs()
 
 if __name__ == "__main__":
-    asyncio.run(worker_loop())
+    start_worker()
+
+
+
+
+
+
+
+
+
+
+
+# import asyncio
+# from worker.queue import redis_client, QUEUE_NAME
+# from worker.job_handler import process_job
+
+
+# async def worker_loop():
+#     print("Worker started. Waiting for jobs...")
+
+#     while True:
+#         job = redis_client.brpop(QUEUE_NAME, timeout=5)
+
+#         if job:
+#             _, job_id = job
+#             await process_job(job_id)
+#         else:
+#             await asyncio.sleep(1)
+
+
+# if __name__ == "__main__":
+#     asyncio.run(worker_loop())
---
+++ worker/web.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+import threading
+from worker.main import start_worker
+
+app = FastAPI()
+
+@app.get("/")
+def health():
+    return {"status": "worker running"}
+
+def run_worker():
+    start_worker()
+
+# Start Redis worker in background thread
+threading.Thread(target=run_worker, daemon=True).start()
