42fcde187caf4db16cd5702601009f7e4c351336
Rishiraj-24 <rishirajcodes.23297020@gmail.com>
2025-12-30 20:59:05 +0530

    fixed backend db

--- backend/app/db/mongodb.py
+++ backend/app/db/mongodb.py
@@ -1,6 +1,19 @@
 from motor.motor_asyncio import AsyncIOMotorClient 
 from app.core.config import settings
+import os
+import ssl
 
-client = AsyncIOMotorClient(settings.MONGODB_URI)
+
+MONGODB_URI=settings.MONGODB_URI
+
+ssl_context = ssl.create_default_context()
+ssl_context.check_hostname = False
+ssl_context.verify_mode = ssl.CERT_NONE
+
+client = AsyncIOMotorClient(
+    MONGODB_URI,
+    tls=True,
+    ssl_context=ssl_context,
+)
 
 db = client.youtube_clipper
